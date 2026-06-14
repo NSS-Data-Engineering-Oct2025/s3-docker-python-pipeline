a725f40e9190de8c72308ae43b9f0aa97e7eed55
zia-da-13 <amerkhyl123@gmail.com>
2026-06-14 12:46:37 -0500

    Dockerize NYC taxi ETL pipeline with AWS SSO support

--- src/upload_to_s3.py
+++ src/upload_to_s3.py
@@ -9,7 +9,7 @@ def upload_processed_files_to_s3():
     Upload all processed CSV files to the project folder in S3.
     """
 
-    session = boto3.Session(profile_name=AWS_PROFILE)
+    session = boto3.Session(profile_name=AWS_PROFILE, region_name="us-east-2")
     s3_client = session.client("s3")
 
     for file_name in os.listdir(PROCESSED_FOLDER):
